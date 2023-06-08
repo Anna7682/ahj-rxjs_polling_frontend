e4b4ba10457118ce9dc574475ab9db1e96771164
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-06-08 21:05:23 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,7 @@ import Widget from './Widget';
 
 const widget = new Widget(
   document.getElementById('root'),
-  'https://ahj-rxjs-polling-redhead80.herokuapp.com/messages/unread',
+  'https://ahj-rxjs-polling-backend.onrender.com',
 );
 
 widget.subscribeToUpdate();
